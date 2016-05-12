2cae54f658c958ea537bb3e1be343e4c163eb517
Toshiki Inami <09c0507@gmail.com>
2016-05-12 17:54:44 +0900

    Add Username/Password login

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,19 @@
 FROM ubuntu:trusty
 
-RUN apt-get update
+ENV DEBIAN_FRONTEND noninteractive
 
-RUN apt-get install -y openssh-server
+RUN apt-get -qq update
+RUN apt-get install -yqq openssh-server
+
 RUN mkdir -p /var/run/sshd
 
 ADD start.sh /start.sh
-RUN chmod +x /*.sh
+RUN chmod +x /start.sh
+
+RUN echo 'root:root' |chpasswd
+
+RUN sed -i "s/PermitRootLogin.*/PermitRootLogin yes/g" /etc/ssh/sshd_config
+RUN sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 
 ENV AUTHORIZED_KEY none
 
--- start.sh
+++ start.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 if [ "$AUTHORIZED_KEY" != "none" ]; then
-    echo "=> Found authorized keys"
     mkdir -p /root/.ssh
     chmod 700 /root/.ssh
     touch /root/.ssh/authorized_keys
     chmod 600 /root/.ssh/authorized_keys
-
     echo "$AUTHORIZED_KEY" >> /root/.ssh/authorized_keys
+
+    # Configure sshd.conf
+    ## Only pubkey authentication
+    sed -i "s/UsePAM.*/UsePAM no/g" /etc/ssh/sshd_config
+    sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
 fi
 
 /usr/sbin/sshd -D
